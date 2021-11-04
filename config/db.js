abf30ec399337dfae338ef5a9cdf62d80f15aeec
AyeshaAzim <ashaik89@my.centennialcollege.ca>
2021-11-03 22:40:34 -0400

    Fixed Database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
     //"URI":"mongodb://localhost/stock",
-    "URI":"mongodb+srv://ayesha:eaN8EW7YCpIvBElt@mongodbserver.un1gg.mongodb.net/stock?retryWrites=true&w=majority" //URI to database on azure
+    "URI":"mongodb+srv://ayesha:SksboViN0Hk35BRh@mongodbserver.un1gg.mongodb.net/stock?retryWrites=true&w=majority" //URI to database on azure
 }
